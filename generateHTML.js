1f30f8d1ac869fbb42f3fa2cf7464bc9ca0787f7
Saidnazar97 <lifedada98@gmail.com>
2020-06-24 06:47:17 -0700

    final work

--- generateHTML.js
+++ generateHTML.js
@@ -6,12 +6,12 @@ const puppeteer = require('puppeteer');
 const readFilePromise = util.promisify(fs.readFile);
 const writeFilePromise = util.promisify(fs.writeFile);
 
-const generateFileName = function (fileExtension = 'html') {
-  return `./_${(new Date).getTime()}.${fileExtension}`;
+const generateFileName = function(fileExtension = 'html') {
+	return `./_${(new Date).getTime()}.${fileExtension}`;
 }
 
-const easy = async function (username, color) {
-  const htmlString = `
+const easy = async function(username, color) {
+	const htmlString = `
 		<!DOCTYPE html>
 		<html lang="en">
 		<head>
@@ -26,39 +26,36 @@ const easy = async function (username, color) {
 		</html>
 	`;
 
-  await writeFilePromise(generateFileName(), htmlString);
+	await writeFilePromise(generateFileName(), htmlString);
 }
 
-const medium = async function (username, color) {
-  const template = await readFilePromise("./template.html", "utf-8");
-  const htmlString = template.replace(new RegExp('\\#\\[username\\]\\#', 'g'), username)
-    .replace(new RegExp('\\#\\[color\\]\\#', 'g'), color);
+const medium = async function(username, color) {
+	const template = await readFilePromise("./template.html", "utf-8");
+	const htmlString = template.replace(new RegExp('\\#\\[username\\]\\#', 'g'), username)
+							.replace(new RegExp('\\#\\[color\\]\\#', 'g'), color);
 
-  await writeFilePromise(generateFileName(), htmlString);
+	await writeFilePromise(generateFileName(), htmlString);
 }
 
-const hard = async function (username, data) {
-  const template = await readFilePromise("./template.ejs", "utf-8");
-  const htmlString = ejs.render(template, {
-    username,
-    data
-  });
+const hard = async function(username, data) {
+	const template = await readFilePromise("./template.ejs", "utf-8");
+	const htmlString = ejs.render(template, {
+		username,
+		data
+	});
 
-  await writeFilePromise(generateFileName(), htmlString);
+	await writeFilePromise(generateFileName(), htmlString);
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setContent(htmlString);
-  await page.pdf({
-    path: generateFileName('pdf'),
-    format: 'A4'
-  });
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.setContent(htmlString);
+	await page.pdf({path: generateFileName('pdf'), format: 'A4'});
 
-  await browser.close();
+	await browser.close();
 }
 
 module.exports = {
-  easy,
-  medium,
-  hard
-};
+	easy,
+	medium,
+	hard
+}
